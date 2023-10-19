a9f86d8b9a5fe8db12df6123b492ad23260424f3
Артем Журкин <artyom.jurkin@yandex.ru>
2023-10-19 22:43:50 +0300

    finished searching logic, watchlist remains

--- data.js
+++ data.js
@@ -6,11 +6,46 @@ movieSearchEl.addEventListener("submit", function (e) {
 
   const movieNameEl = document.getElementById("movie-name");
   movieListEl.innerHTML = "";
-  getFilmSearchResults(movieNameEl.value);
+  handleFilmSearchResults(movieNameEl.value);
 });
 
-function getFilmSearchResults(movieName) {
+function handleFilmSearchResults(movieName) {
   fetch(`http://www.omdbapi.com/?apikey=c0791940&t=${movieName}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      if (data.Response === 'False') {
+        throw Error("Movie not found");
+      } else {
+        renderFilmCard(data);
+      }
+    })
+    .catch(() => movieListEl.innerHTML = `<p>Unable to find what you're looking for. Please try another search.</p>`)
+};
+
+function renderFilmCard(data) {
+  const { Poster, Title, Ratings, Runtime, Genre, Plot } = data;
+
+  const filmCard = document.createElement("div");
+  filmCard.classList.add("film-card");
+  filmCard.innerHTML =
+    `
+    <div class="poster-div">
+      <img class="poster-img" src=${Poster}>
+    </div>
+    <div class="description-field">
+      <div class="d-flex">
+        <h3 class="title">${Title}</h3>
+        <p class="raiting"><span>&#9733</span> ${Ratings[0].Value}
+      </div>
+      <div class="d-flex">
+        <p>${Runtime}</p>
+        <p>${Genre}</p>
+        <button id="add-to-watchlist">+</button>
+      </div>
+      <div>
+        <p>${Plot}</p>
+      </div>
+    </div>
+  `
+  movieListEl.append(filmCard);
 }
